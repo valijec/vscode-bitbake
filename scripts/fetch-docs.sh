46308fda3eebca0662fd405186bf6604caebf85d
valijec <valijec@users.noreply.github.com>
2024-09-01 01:13:59 +0000

    Auto update references for commits, tags, etc

--- scripts/fetch-docs.sh
+++ scripts/fetch-docs.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Tag: yocto-5.0.1
-BITBAKE_DOCS_COMMIT=8f90d10f9efc9a32e13f6bd031992aece79fe7cc
-# Tag: yocto-5.0.1
-YOCTO_DOCS_COMMIT=875dfe69e93bf8fee3b8c07818a6ac059f228a13
+# Tag: yocto-5.0.3
+BITBAKE_DOCS_COMMIT=11d83170922a2c6b9db1f6e8c23e533526984b2c
+    (cherry picked from commit 1e6f862864539d6f6a0bea3e4479e0dd40ff3091)
+# Tag: yocto-5.0.3
+YOCTO_DOCS_COMMIT=09e321d7cbd5bd7071a0c138e6d0c424fb086269
++-  Git Revision: :yocto_git:`f7def85be9f99dcb4ba488bead201f670304379b </poky/commit/?id=f7def85be9f99dcb4ba488bead201f670304379b>`
++-  Git Revision: :oe_git:`803cc32e72b4fc2fc28d92090e61f5dd288a10cb </openembedded-core/commit/?id=803cc32e72b4fc2fc28d92090e61f5dd288a10cb>`
++-  Git Revision: :yocto_git:`acbba477893ef87388effc4679b7f40ee49fc852 </meta-mingw/commit/?id=acbba477893ef87388effc4679b7f40ee49fc852>`
++-  Git Revision: :oe_git:`8714a02e13477a9d97858b3642e05f28247454b5 </bitbake/commit/?id=8714a02e13477a9d97858b3642e05f28247454b5>`
++-  Git Revision: :yocto_git:`875dfe69e93bf8fee3b8c07818a6ac059f228a13 </yocto-docs/commit/?id=875dfe69e93bf8fee3b8c07818a6ac059f228a13>`
 
 BITBAKE_DOCS_LIST="bitbake-user-manual-metadata.rst bitbake-user-manual-ref-variables.rst"
 YOCTO_DOCS_LIST=" tasks.rst variables.rst"
